ab29b2616cf79b254459df338c0ea3fcbf7a17b1
johan-rain <johan@ranestam.se>
2023-01-18 13:52:41 +0100

    changed toggleShowFront to useCallback, not creating new func on every render + some CSS

--- src/components/Flippable-card.jsx
+++ src/components/Flippable-card.jsx
@@ -1,23 +1,23 @@
 import '../assets/scss/App.scss';
 import QuestionList from './QuestionList';
 import {CSSTransition} from 'react-transition-group';
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 
 function FlippableCard() {
-    const [showFront, setShowFront] = useState(true);
-    return(
-        <div className="flippable-card-container">
-            <CSSTransition
-                in={showFront}
-                timeout={300}
-                classNames='flip'
-            >
-                <QuestionList onClick={() => {
-                    setShowFront((v) => !v);
-                }}/>
-            </CSSTransition>
-        </div>
-    );
+	const [showFront, setShowFront] = useState(true);
+	const toggleShowFront = useCallback(() => { setShowFront((v) => !v) }, [setShowFront])
+
+	return(
+		<div className="flippable-card-container">
+			<CSSTransition
+				in={showFront}
+				timeout={300}
+				classNames='flip'
+			>
+				<QuestionList onClick={toggleShowFront}/>
+			</CSSTransition>
+		</div>
+	);
 }
 
 export default FlippableCard;
